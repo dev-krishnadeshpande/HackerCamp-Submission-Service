34a4c0d70deddc6cf2f0a0a4ae970538c818216e
dev-krishnadeshpande <krishnadeshpande.dev@gmail.com>
2025-02-23 00:07:50 +0530

    Added evaluation worker

--- src/index.js
+++ src/index.js
@@ -2,17 +2,18 @@ const fastify = require('fastify')({ logger: true });
 const app = require('./app');
 const connectToDB = require('./config/dbConfig');
 const { PORT } = require('./config/serverConfig');
+const { EVALUATION_QUEUE } = require('./utils/constants');
+const EvaluationWorker = require('./workers/EvaluationWorker');
 
 fastify.register(app);
 
 fastify.listen({ port: PORT }, function () {
   try {
     connectToDB();
+    EvaluationWorker(EVALUATION_QUEUE);
   }
   catch (error) {
-    console.log('error', error);
+    console.error('error', error);
   }
-
-  // console.log(`Server up at port ${port}`);
 })
 
